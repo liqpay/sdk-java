7c923d0232c1255e7d8da397edd1ee1dedc72ace
Sergey Ponomarev <stokito@gmail.com>
2015-02-16 13:14:36 +0200

    LiqPayUtilTest.java

--- src/test/java/com/liqpay/LiqPayUtilTest.java
+++ src/test/java/com/liqpay/LiqPayUtilTest.java
@@ -9,9 +9,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import static com.liqpay.LiqPayUtil.base64_encode;
-import static com.liqpay.LiqPayUtil.getArray;
-import static com.liqpay.LiqPayUtil.sha1;
+import static com.liqpay.LiqPayUtil.*;
 import static org.junit.Assert.*;
 
 public class LiqPayUtilTest {
@@ -64,5 +62,34 @@ public class LiqPayUtilTest {
         assertEquals(42, parsedInnerJsonObject.get("intField"));
         assertEquals(Long.MAX_VALUE, parsedInnerJsonObject.get("longField"));
         assertEquals(33.3D, parsedInnerJsonObject.get("doubleField"));
+    }
+
+    @Test
+    public void testParseJson() throws ParseException {
+        // given:
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("stringField", "value");
+        jsonObject.put("intField", 42);
+        jsonObject.put("longField", Long.MAX_VALUE);
+        jsonObject.put("doubleField", 33.3D);
+        JSONObject innerJsonObject = new JSONObject();
+        innerJsonObject.put("stringField", "value");
+        jsonObject.put("innerJsonObject", innerJsonObject);
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add("string 1");
+        jsonArray.add("string 2");
+        jsonObject.put("array", jsonArray);
+        // when:
+        Map parsed = parseJson(jsonObject);
+        // then:
+        assertEquals("value", parsed.get("stringField"));
+        assertEquals(42, parsed.get("intField"));
+        assertEquals(Long.MAX_VALUE, parsed.get("longField"));
+        assertEquals(33.3D, parsed.get("doubleField"));
+        Map parsedInnerJsonObject = (Map) parsed.get("innerJsonObject");
+        assertEquals("value", parsedInnerJsonObject.get("stringField"));
+        List parsedArray = (List) parsed.get("array");
+        assertEquals("string 1", parsedArray.get(0));
+        assertEquals("string 2", parsedArray.get(1));
     }
 }
